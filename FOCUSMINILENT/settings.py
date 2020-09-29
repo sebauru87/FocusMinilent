8314a86663ea038e4ed8dfcf4eb5b6e5832275e9
Sebastian <Ferreira>
2020-09-29 09:51:49 -0300

    heroku deploy

--- FOCUSMINILENT/settings.py
+++ FOCUSMINILENT/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '82aqi_@mvij*98n1snw=id)nwjp(*itkf(_4fs)!4q2_debm_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['focusminilent.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn FOCUSMINILENT.wsgi
